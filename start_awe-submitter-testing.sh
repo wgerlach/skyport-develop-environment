c2fc5013387d4eb42a2fc10ff58f56a7870f9976
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-09-26 11:29:12 -0500

    add urls for shock and awe server

--- start_awe-submitter-testing.sh
+++ start_awe-submitter-testing.sh
@@ -19,4 +19,4 @@ source /Users/wolfganggerlach/git/Skyport2/skyport2.env
 export NAME=awe-submitter
 export DATADIR=/Users/wolfganggerlach/awe_data
 docker rm -f ${NAME} > /dev/null 2>&1
-docker run -ti --network skyport2_default --name ${NAME} --add-host skyport.local:${SKYPORT_DOCKER_GATEWAY} --workdir=/go/src/github.com/MG-RAST/AWE -v ${DATADIR}:${DATADIR}  -v /Users/wolfganggerlach/gopath/src:/go/src -v /Users/wolfganggerlach/git/Skyport2/live-data/env/:/skyport2-env/:ro  mgrast/awe-submitter-testing ash
+docker run -ti --network skyport2_default --name ${NAME} --add-host skyport.local:${SKYPORT_DOCKER_GATEWAY} -e SHOCK_SERVER="http://skyport.local:8001/shock/api/" -e AWE_SERVER="http://skyport.local:8001/awe/api/" --workdir=/go/src/github.com/MG-RAST/AWE -v ${DATADIR}:${DATADIR}  -v /Users/wolfganggerlach/gopath/src:/go/src -v /Users/wolfganggerlach/git/Skyport2/live-data/env/:/skyport2-env/:ro  mgrast/awe-submitter-testing ash
